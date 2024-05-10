54797d33a2366f69e1775591714e648ef361f9ae
Alex <Disblocker.just@gmail.com>
2024-05-11 01:33:43 +0300

    inbetween commit

---
+++ main_types/workout_info.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "main_types.h"
+#include <unordered_set>
+#include <boost/container_hash/hash.hpp>
+
+typedef struct 
+{
+    int student_id;
+    Lesson real_lesson;
+    std::tm real_attend;
+    Lesson should_lesson;
+    std::tm should_attend;
+
+    bool operator==(const Workout_Info_ &other) const
+    {
+        return (student_id == other.student_id
+            && real_attend == other.real_attend
+            && should_attend == other.should_attend
+            && real_lesson == other.real_lesson
+            && should_lesson == other.should_lesson);
+    }
+} Workout_Info_, *Workout_Info_Ptr;
+
+struct Real_Workout_Hash
+{
+    std::size_t operator()(const Workout_Info_Ptr& s) const noexcept
+    {
+        std::size_t seed = 0;
+        boost::hash_combine(seed, s->student_id);
+        boost::hash_combine(seed, s->real_attend.tm_mon);
+        boost::hash_combine(seed, s->real_attend.tm_mday);
+        boost::hash_combine(seed, s->real_lesson.merged_lesson_id);
+        boost::hash_combine(seed, s->real_lesson.internal_lesson_id);
+        return seed;
+    }
+};
+
+struct Should_Workout_Hash
+{
+    std::size_t operator()(const Workout_Info_Ptr& s) const noexcept
+    {
+        std::size_t seed = 0;
+        boost::hash_combine(seed, s->student_id);
+        boost::hash_combine(seed, s->should_attend.tm_mon);
+        boost::hash_combine(seed, s->should_attend.tm_mday);
+        boost::hash_combine(seed, s->should_lesson.merged_lesson_id);
+        boost::hash_combine(seed, s->should_lesson.internal_lesson_id);
+        return seed;
+    }
+};
