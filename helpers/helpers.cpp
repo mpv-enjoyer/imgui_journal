a62d222b006d43fd26f3db11969ca47848e5587c
Alex <Disblocker.just@gmail.com>
2024-11-24 04:03:58 +0300

    remove old j_... definitions from helpers.cpp

--- helpers/helpers.cpp
+++ helpers/helpers.cpp
@@ -61,34 +61,6 @@ std::string to_string(const std::vector<std::string>& strings, std::string separ
     return output;
 }
 
-bool j_button_selectable(const char* label, bool selected, bool small)
-{
-    if (selected)
-    {
-        ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(2.0f / 7.0f, 0.6f, 0.6f));
-        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(2.0f / 7.0f, 0.7f, 0.7f));
-        ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(2.0f / 7.0f, 0.8f, 0.8f));
-    }
-    bool output;
-    if (!small) output = ImGui::Button(label);
-    else output = ImGui::SmallButton(label);
-    if (selected)
-    {
-        ImGui::PopStyleColor(3);
-    }
-    return output;
-}
-
-bool j_button_dangerous(const char* label)
-{
-    ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(7.0f / 7.0f, 0.7f, 0.7f));
-    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(7.0f / 7.0f, 0.8f, 0.8f));
-    ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(7.0f / 7.0f, 0.9f, 0.9f));
-    bool output = ImGui::Button(label);
-    ImGui::PopStyleColor(3);
-    return output;
-}
-
 int validate_time_int(std::string input_string, int upper_limit)
 {
     if (input_string.size() == 0) return -1;
@@ -102,50 +74,6 @@ int validate_time_int(std::string input_string, int upper_limit)
     return output;
 }
 
-bool j_input_time(std::string label, JTime& time)
-{
-    bool result = false;
-    std::string pre_input_buffer;
-    char input_buffer[3];
-    if (ImGui::BeginTable(label.c_str(), 3, ImGuiTableFlags_NoHostExtendX | ImGuiTableFlags_PreciseWidths | ImGuiTableFlags_NoPadInnerX | ImGuiTableFlags_NoPadOuterX | ImGuiTableFlags_SizingFixedFit))
-    {
-        ImGui::TableNextColumn();
-        ImGui::SetNextItemWidth(ImGui::CalcTextSize("8888").x);
-        pre_input_buffer = std::to_string(time.hours); if (pre_input_buffer.size() == 1) pre_input_buffer = "0" + pre_input_buffer;
-        strncpy(input_buffer, pre_input_buffer.c_str(), 3);
-        IM_ASSERT(input_buffer[2] == '\0' || input_buffer[1] == '\0' || input_buffer[0] == '\0');
-        std::string hours_name = generate_label(label, { 0 });
-        if (ImGui::InputText(hours_name.c_str(), input_buffer, IM_ARRAYSIZE(input_buffer), ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsDecimal))
-        {
-            int new_hours = validate_time_int(std::string(input_buffer), 24);
-            if (new_hours != -1) 
-            {
-                time.hours = new_hours;
-                result = true;
-            }
-        }
-        ImGui::TableNextColumn();
-        ImGui::Text(":");
-        ImGui::TableNextColumn();
-        ImGui::SetNextItemWidth(ImGui::CalcTextSize("8888").x);
-        pre_input_buffer = std::to_string(time.minutes); if (pre_input_buffer.size() == 1) pre_input_buffer = "0" + pre_input_buffer;
-        strncpy(input_buffer, pre_input_buffer.c_str(), 3);
-        IM_ASSERT(input_buffer[2] == '\0' || input_buffer[1] == '\0' || input_buffer[0] == '\0');
-        std::string minutes_name = generate_label(label, { 1 });
-        if (ImGui::InputText(minutes_name.c_str(), input_buffer, IM_ARRAYSIZE(input_buffer), ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_CharsDecimal))
-        {
-            int new_minutes = validate_time_int(std::string(input_buffer), 60);
-            if (new_minutes != -1)
-            {
-                time.minutes = new_minutes;
-                result = true;
-            }
-        }
-        ImGui::EndTable();
-    }
-    return result;
-}
-
 std::string generate_label(const std::string prefix, std::vector<int> unique)
 {
     std::string output = prefix;
@@ -154,113 +82,6 @@ std::string generate_label(const std::string prefix, std::vector<int> unique)
         output.append("." + std::to_string(unique[i]));
     }
     return output;
-}
-
-bool j_age_group_combo(const char* label, int* age_group, bool shrink)
-{
-    const char* combo_preview_value = Age_Group_Names[*age_group].c_str();
-    ImGuiComboFlags flags = ImGuiComboFlags_HeightLargest;
-    if (shrink) flags |= ImGuiComboFlags_WidthFitPreview;
-    if (ImGui::BeginCombo(label, combo_preview_value, flags))
-    {
-        for (int n = 0; n < Age_Group_Names.size(); n++)
-        {
-            const bool is_selected = (*age_group == n);
-            if (ImGui::Selectable(Age_Group_Names[n].c_str(), is_selected))
-            {
-                *age_group = n;
-                ImGui::EndCombo();
-                return true;
-            }
-            // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-            if (is_selected)
-                ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-    }
-    return false;
-};
-
-bool j_attendance_combo(const char* label, int* status, std::string tooltip)
-{
-    ImGui::SetNextItemWidth(SUBCOLUMN_WIDTH_PXLS);
-    //int dummy = 0;
-    const char* items[] = { " ", "V", "Б", "O", "ОТР" };
-    //if (*status == STATUS_WORKED_OUT)
-    //{
-        //ImVec2 gradient_size = ImVec2(SUBCOLUMN_WIDTH_PXLS, ImGui::GetFrameHeight());
-        //ImGui::InvisibleButton(label, gradient_size);
-        //ImGui::BeginDisabled();
-        //if (ImGui::BeginCombo("##OTR", "OTP", ImGuiComboFlags_NoArrowButton)) ImGui::EndCombo();
-        //ImGui::EndDisabled();
-        
-        //return false;
-    //}
-    if (*status == STATUS_NOT_AWAITED)
-    {
-        ImVec2 gradient_size = ImVec2(SUBCOLUMN_WIDTH_PXLS, ImGui::GetFrameHeight());
-        {
-            ImVec2 p0 = ImGui::GetCursorScreenPos();
-            ImVec2 p1 = ImVec2(p0.x + gradient_size.x, p0.y + gradient_size.y);
-            ImDrawList* draw_list = ImGui::GetWindowDrawList();
-            draw_list->AddRectFilled(p0, p1, IM_COL32(135, 135, 135, 255));
-            ImGui::InvisibleButton("##gradient1", gradient_size);
-        }
-        return false;
-    }
-    const char* combo_preview_value = items[*status];  // Pass in the preview value visible before opening the combo (it could be anything)
-    bool modify_for_workout = *status == STATUS_WORKED_OUT;
-    ImGuiComboFlags flags = modify_for_workout ? ImGuiComboFlags_NoArrowButton : 0; 
-    if (ImGui::BeginCombo(label, combo_preview_value, flags))
-    {
-        for (int n = 0; n < IM_ARRAYSIZE(items) - (!modify_for_workout); n++)
-        {
-            const bool is_selected = (*status == n);
-            if (ImGui::Selectable(items[n], is_selected))
-            {
-                *status = n;
-                ImGui::EndCombo();
-                return true;
-            }
-            // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-            if (is_selected)
-                ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-    }
-    if (*status == STATUS_WORKED_OUT) ImGui::SetItemTooltip(tooltip.c_str());
-    return false;
-};
-
-bool j_attend_data(std::string label, Attend_Data* attend_data, std::string first_lesson_name, std::string second_lesson_name)
-{
-    std::string lesson_concat = to_string({ first_lesson_name, second_lesson_name}, "+").c_str();
-    if (first_lesson_name == second_lesson_name)
-    {
-        first_lesson_name += " 1";
-        second_lesson_name += " 2";
-    }
-    const char* items[] = {lesson_concat.c_str(), first_lesson_name.c_str(), second_lesson_name.c_str()};
-    const char* combo_preview_value = items[*attend_data];  // Pass in the preview value visible before opening the combo (it could be anything)
-    //ImGui::SetNextItemWidth(SUBCOLUMN_WIDTH_PXLS);
-    if (ImGui::BeginCombo(label.c_str(), combo_preview_value, ImGuiComboFlags_WidthFitPreview))
-    {
-        for (int n = 0; n < IM_ARRAYSIZE(items); n++)
-        {
-            const bool is_selected = (*attend_data == n);
-            if (ImGui::Selectable(items[n], is_selected))
-            {
-                *attend_data = n;
-                ImGui::EndCombo();
-                return true;
-            }
-            // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-            if (is_selected)
-                ImGui::SetItemDefaultFocus();
-        }
-        ImGui::EndCombo();
-    }
-    return false;
 }
 
 template <typename T = int, const Student&>
@@ -397,13 +218,4 @@ std::tm Time_Archiver::to_tm()
     tm.tm_wday = wday;
     tm.tm_yday = year;
     return tm;
-}
-
-bool j_button_colored(const char* label, float r, float g, float b)
-{
-    ImVec4 color(r, g, b, 1.0f);
-    ImGui::PushStyleColor(ImGuiCol_Button, color);
-    bool result = ImGui::Button(label);
-    ImGui::PopStyleColor();
-    return result;
 }
