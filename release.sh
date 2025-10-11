1c01ad43a351c40fac9a205e561f519a43d270f5
Alex <Disblocker.just@gmail.com>
2025-10-11 03:53:44 +0300

    WIN32: add relevant dlls to the installer

--- release.sh
+++ release.sh
@@ -1,10 +1,16 @@
-rm AttendanceJournal.zip
+set -xe
+rm AttendanceJournal.zip -f
 cd build
 make clean
 make WIN32=True
 yes | cp -f journal.exe Release/
 cd Release
-rm *.data
+rm *.data -f
 rm backup/* -rf
+rm libgcc_s_dw2-1.dll libstdc++-6.dll libwinpthread-1.dll -f
+WiN32LIB_FOLDER="/usr/i686-w64-mingw32/bin/"
+cp $WiN32LIB_FOLDER/libgcc_s_dw2-1.dll  .
+cp $WiN32LIB_FOLDER/libstdc++-6.dll     .
+cp $WiN32LIB_FOLDER/libwinpthread-1.dll .
 echo "Dummy" > backup/Dummy.txt
 zip ../../AttendanceJournal.zip -r * 
